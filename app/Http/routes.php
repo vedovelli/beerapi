5a7e2c3b393cd5e707bbbf0c6a6664bf2c876525
Vedovelli <vedovelli@gmail.com>
2015-08-13 12:34:52 -0300

    Adicionadas rotas em ingles

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,7 +9,17 @@ Route::get('cervejarias', ['middleware' => 'cors', function()
     return \Response::json(\App\Brewery::with('beers', 'geocode')->paginate(10), 200);
 }]);
 
+Route::get('breweries', ['middleware' => 'cors', function()
+{
+    return \Response::json(\App\Brewery::with('beers', 'geocode')->paginate(10), 200);
+}]);
+
 Route::get('cervejas', ['middleware' => 'cors', function()
+{
+    return \Response::json(\App\Beer::with('brewery')->paginate(10), 200);
+}]);
+
+Route::get('beers', ['middleware' => 'cors', function()
 {
     return \Response::json(\App\Beer::with('brewery')->paginate(10), 200);
 }]);
